27ab3b757fd189c038a24c2647d95a908723ab47
ArmageddonLP <armaggedonlp@gmail.com>
2023-01-26 21:05:03 +0100

    feat: add status and fix install

--- prog-graph.sh
+++ prog-graph.sh
@@ -3,9 +3,13 @@ function invalid {
   echo ""
   echo "Invalid command"
   echo ""
-  echo "Create local postgres container:  prog_graph.sh [local]"
-  echo "Install dependencies:             prog_graph.sh [install]"
-  echo "Docker services:                  prog_graph.sh [build|start|stop] [dev|prod]"
+  echo "Create local postgres container:  prog-graph.sh [local]"
+  echo "Install dependencies:             prog-graph.sh [install]"
+  echo "Docker services:                  prog-graph.sh [build|start|stop|restart] [dev|prod]"
+  echo "Docker status:                    prog-graph.sh [status]"
+}
+function status () {
+  docker container ls --all
 }
 function localPostgresDocker () {
   docker run --name postgres-db -e POSTGRES_PASSWORD=pw_postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=prog_graph -p 5432:5432 -d postgres:latest
@@ -14,12 +18,12 @@ function localPostgresDocker () {
 function installDependencies () {
   sudo apt-get update -y
   sudo apt-get upgrade -y
-  sudo apt-get install docker -y
+  sudo curl -fsSL https://get.docker.com | sudo sh
   sudo apt autoremove -y
   sudo docker pull postgres
   sudo groupadd docker
   sudo usermod -aG docker $USER
-  sudo shutdown -r now
+  sudo reboot
 }
 function exec {
   if [ $1 = build ]
@@ -29,8 +33,10 @@ function exec {
   else
     docker compose --file docker/docker-compose_$2.yml --project-name prog-graph_$2 $1
   fi
+  docker container ls --all
 }
 case $1 in
+status)status;;
 local)localPostgresDocker;;
 install)installDependencies;;
 build)
@@ -49,6 +55,12 @@ stop)
   case $2 in
     dev)exec stop dev;;
     prod)exec stop prod;;
+    *)invalid;;
+  esac;;
+restart)
+  case $2 in
+    dev)exec restart dev;;
+    prod)exec restart prod;;
     *)invalid;;
   esac;;
 *)invalid;;
