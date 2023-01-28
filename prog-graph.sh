731c9f4b3b6decf4e2defd4ce3b43f440c80d513
ArmageddonLP <armaggedonlp@gmail.com>
2023-01-28 11:41:54 +0100

    feat: environments - changed environments to be test, int and prod instead of dev and prod

--- prog-graph.sh
+++ prog-graph.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-function invalid {
+function help {
   echo ""
-  echo "Invalid command"
-  echo ""
-  echo "Create local postgres container:  prog-graph.sh [local]"
-  echo "Install dependencies:             prog-graph.sh [install]"
-  echo "Docker services:                  prog-graph.sh [build|start|stop|restart] [dev|prod]"
+  echo "Help:                             prog-graph.sh [help]"
+  echo "Setup for use on linux:           prog-graph.sh [setup]"
+  echo "Create postgres dev db:           prog-graph.sh [dev] [db]"
+  echo "Docker build:                     prog-graph.sh [build] [test|int|prod]"
+  echo "Docker start:                     prog-graph.sh [start] [test|int|prod]"
+  echo "Docker stop:                      prog-graph.sh [stop] [test|int|prod]"
+  echo "Docker restart:                   prog-graph.sh [restart] [test|int|prod]"
+  echo "Docker stop-build-start:          prog-graph.sh [rebuild] [test|int|prod]"
+  echo "Docker logs:                      prog-graph.sh [logs] [test|int|prod]"
   echo "Docker status:                    prog-graph.sh [status]"
   echo ""
 }
-function status () {
-  docker container ls --all
+function invalid {
+  echo ""
+  echo "Invalid command"
+  help
 }
-function localPostgresDocker () {
-  docker run --name postgres-db -e POSTGRES_PASSWORD=pw_postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=prog_graph -p 5432:5432 -d postgres:latest
-  docker stop postgres-db
-}
-function installDependencies () {
+function setup {
   sudo apt-get update -y
   sudo apt-get upgrade -y
   sudo curl -fsSL https://get.docker.com | sudo sh
@@ -26,43 +28,91 @@ function installDependencies () {
   sudo usermod -aG docker $USER
   sudo reboot
 }
-function exec {
-  if [ $1 = build ]
-  then
-    docker build --file docker/docker_app --tag prog-graph_app-$2 --build-arg SYSTEM_ENV=$2 .
-    docker compose --file docker/docker-compose_$2.yml --project-name prog-graph_$2 create
-  else
-    docker compose --file docker/docker-compose_$2.yml --project-name prog-graph_$2 $1
-  fi
+function dev {
+  docker run --name db-dev -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=prog-graph -p 5432:5432 -d postgres:latest postgres -c log_statement=all
+  docker stop db-dev
+}
+function build {
+  docker build --file docker/docker_app --tag prog-graph_app-$1 --build-arg SYSTEM_ENV=$1 .
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 create
+  status
+}
+function start {
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 start
+  status
+}
+function stop {
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 stop
+  status
+}
+function restart {
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 restart
+  status
+}
+function rebuild {
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 stop
+  docker build --file docker/docker_app --tag prog-graph_app-$1 --build-arg SYSTEM_ENV=$1 .
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 create
+  docker compose --file docker/docker-compose_$1.yml --project-name prog-graph_$1 start
+  status
+}
+function logs {
+  docker logs -f app-$1 2>&1 | sed -e 's/^/ [-- APP --] /' &
+  docker logs -f db-$1 2>&1 | sed -e 's/^/ [-- DB --] /' &
+}
+function status {
   docker container ls --all
 }
 case $1 in
-status)status;;
-local)localPostgresDocker;;
-install)installDependencies;;
-build)
-  case $2 in
-    dev)exec build dev;;
-    prod)exec build prod;;
-    *)invalid;;
-  esac;;
-start)
-  case $2 in
-    dev)exec start dev;;
-    prod)exec start prod;;
-    *)invalid;;
-  esac;;
-stop)
-  case $2 in
-    dev)exec stop dev;;
-    prod)exec stop prod;;
-    *)invalid;;
-  esac;;
-restart)
-  case $2 in
-    dev)exec restart dev;;
-    prod)exec restart prod;;
-    *)invalid;;
-  esac;;
-*)invalid;;
+  help)help;;
+  setup)setup;;
+  dev)
+    case $2 in
+      db)dev;;
+      *)invalid;;
+    esac;;
+  build)
+    case $2 in
+      test)build test;;
+      int)build int;;
+      prod)build prod;;
+      *)invalid;;
+    esac;;
+  start)
+    case $2 in
+      test)start test;;
+      int)start int;;
+      prod)start prod;;
+      *)invalid;;
+    esac;;
+  stop)
+    case $2 in
+      test)stop test;;
+      int)stop int;;
+      prod)stop prod;;
+      *)invalid;;
+    esac;;
+  restart)
+    case $2 in
+      test)restart test;;
+      int)restart int;;
+      prod)restart prod;;
+      *)invalid;;
+    esac;;
+  rebuild)
+    case $2 in
+      test)rebuild test;;
+      int)rebuild int;;
+      prod)rebuild prod;;
+      *)invalid;;
+    esac;;
+  logs)
+    case $2 in
+      test)logs test;;
+      int)logs int;;
+      prod)logs prod;;
+      *)invalid;;
+    esac;;
+  status)status;;
+  *)invalid;;
 esac
